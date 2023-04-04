1f1fe01e330daa5dee8c90c9b5e950a410cac3b4
Specter-0 <sigurd.thornes@gmail.com>
2023-04-04 17:02:19 +0200

    we do a little trolling :alien:

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,9 @@
 // functions----------------------------------------------------------------------------
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void processInput(GLFWwindow *window);
+void processInput(GLFWwindow* window);
+void confTriangle(unsigned int VAO, unsigned int VBO, float vertices[]);
+
 
 // shader code-------------------------------------------------------------------------
 
@@ -21,7 +23,7 @@ const char* vertexShaderSource =
     "}\n";
 
 // fragment shader code
-const char *fragmentShaderSource = 
+const char* fragmentShaderSource = 
     "#version 330 core\n"
     "out vec4 FragColor;\n"
     "void main()\n"
@@ -52,16 +54,14 @@ int main() {
         return -1;
     }
     glfwMakeContextCurrent(window);
+    // creates viewport and window resize callback
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     // loades glad and checks if success
     if (!gladLoadGL()) { // can be exchanged with: gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)
         std::cout << "bitch you done fuck it all up! with glad spice" << std::endl;
         return -1;
     }
-
-    // creates viewport and window resize callback
-    glViewport(0, 0, 800, 600);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     // shaders---------------------------------------------------------------------------
 
@@ -70,8 +70,7 @@ int main() {
     GLchar infolog[512];
 
     // vertex shaders and checks if succses
-    unsigned int vertexShader;
-    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
     glCompileShader(vertexShader);
 
@@ -80,11 +79,9 @@ int main() {
         glGetShaderInfoLog(vertexShader, 512, NULL, infolog);
         std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infolog << std::endl;
     }
-    // vertex shaders and checks if succses
 
     // fragment shaders and checks if succses
-    unsigned int fragmentShader;
-    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
     glCompileShader(fragmentShader);
 
@@ -95,10 +92,9 @@ int main() {
     }
 
     // linking shaders and checks if success
-    unsigned int shaderProgram;
-    shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, fragmentShader);
+    unsigned int shaderProgram = glCreateProgram();
     glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
     glLinkProgram(shaderProgram);
 
     glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
@@ -112,29 +108,48 @@ int main() {
     // set up vertex data-----------------------------------------------------------------
 
     //data for triangle
-    float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.0f,  0.5f, 0.0f
+    float vertices[2][9] = {
+        { // first triangle
+            -0.6f, 0.4f, 0.0f, // top 
+            -0.3f, -0.2f, 0.0f, // bottom right
+            -0.9f, -0.2f, 0.0f // bottom left
+        },
+        { // secound triangle
+            0.6f, -0.4f, 0.0f,  // top 
+            0.3f, 0.2f, 0.0f,  // bottom right
+            0.9f, 0.2f, 0.0f,  // bottom left
+        }
     };
 
+    std::cout << vertices << std::endl << vertices[0][0] << std::endl << sizeof(vertices);
+    //std::cout << vertices[0] << std::endl << sizeof(vertices[0]);
+    
     // buffers---------------------------------------------------------------------------
 
-    unsigned int VBO, VAO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);  
+    unsigned int VBOs[2], VAOs[2];
+    glGenVertexArrays(2, VAOs);
+    glGenBuffers(2, VBOs);  
 
-    glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    // configure vertex attributes
-
+    // configure triangles
+    
+    // first triangle setup
+    glBindVertexArray(VAOs[0]);
+    glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices[0]), vertices[0], GL_STATIC_DRAW);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);  
+    glEnableVertexAttribArray(0);
+
+    // second triangle setup
+    glBindVertexArray(VAOs[1]);	
+    glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);	
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices[1]), vertices[1], GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0); 
+    glEnableVertexAttribArray(0);
 
     // window loop-------------------------------------------------------------------------
+
+    // draws in wireframe mode; comment out
+    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); 
 
     while (!glfwWindowShouldClose(window)) {
 
@@ -147,13 +162,23 @@ int main() {
 
         // draw
         glUseProgram(shaderProgram);
-        glBindVertexArray(VAO);
+
+        glBindVertexArray(VAOs[0]);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glBindVertexArray(VAOs[1]);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
         // swap buffers and poll events
         glfwSwapBuffers(window);
         glfwPollEvents();    
     }
+
+    // cleanup------------------------------------------------------------------------------
+
+    glDeleteVertexArrays(2, VAOs);
+    glDeleteBuffers(2, VBOs);
+    glDeleteProgram(shaderProgram);
 
     glfwTerminate();
     return 0;
